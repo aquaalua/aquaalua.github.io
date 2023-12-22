39e8a4c00d55e783d0eae3492dba3aec33f808f8
aquaalua <153616030+aquaalua@users.noreply.github.com>
2023-12-22 13:33:20 +0800

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Script Page</title>
+</head>
+<body>
+    <h1>Script Page</h1>
+    <p>This is the script page.</p>
+
+    <!-- Link back to the home page -->
+    <a href="index.html">Go back to Home Page</a>
+</body>
+</html>
